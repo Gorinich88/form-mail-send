88c6d5384a714a94eb887ed89df158286805be9a
Gorinich88 <hwarang756@gmail.com>
2022-01-07 16:26:18 +0200

    first commit

---
+++ send.php
@@ -0,0 +1,21 @@
+<?php
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$text = $_POST['text'];
+
+$to = "hwarang756@gmail.com"; 
+$date = date ("d.m.Y"); 
+$time = date ("h:i");
+$from = $email;
+$subject = "Заявка c сайта";
+
+$msg="
+Имя: $name /n
+Телефон: $phone /n
+Почта: $email /n
+Текст: $text"; 	
+mail($to, $subject, $msg, "From: $from ");
+?>
+<p>Hello, form send!</p>
+
